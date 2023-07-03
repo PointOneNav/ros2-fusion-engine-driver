13c8c700b6be7df5b9c9063b9cb45492dafb4924
Mblancho <martin.blancho@epitech.eu>
2023-07-03 12:52:46 +0200

    feat(norm and use lower bounds function for leap second)

--- fusion-engine-driver/utils/conversion_utils.hpp
+++ fusion-engine-driver/utils/conversion_utils.hpp
@@ -127,18 +127,6 @@ class ConversionUtils {
     return current_timestamp + 2208988800;
   }
 
-  static int getLastLeapSecondDate(int64_t current_timestamp) {
-      int leap_second_value = 0;
-      std::map<int64_t, int> leap_second = getLeapSeconds();
-
-      for (const auto &ele : leap_second) {
-          if (ele.first <= convertUnixToNtp(current_timestamp)) {
-            leap_second_value = ele.second;
-          }
-      }
-      return leap_second_value;
-  }
-
   /**
    * @brief Convert GPS time to UTC time in string format.
    *
@@ -150,7 +138,7 @@ class ConversionUtils {
     auto currentTime = std::chrono::system_clock::now();
     std::time_t currentTimeT = std::chrono::system_clock::to_time_t(currentTime);
     int64_t currentTimestamp = static_cast<int64_t>(currentTimeT);
-    const int kLeapSecondsOffset = getLastLeapSecondDate(currentTimestamp);
+    const int kLeapSecondsOffset = getLeapSeconds(currentTimestamp);
 
     // Extract GPS time components
     int gps_hours = static_cast<int>((gps_time / 3600.0));
@@ -245,39 +233,52 @@ class ConversionUtils {
     return nmea;
   }
 
-  // static std::map<int, int> leap_seconds_;
-
-  static const std::map<int64_t, int> &getLeapSeconds() {
-    static const std::map<int64_t, int> leap_seconds = {
-      {2272060800, 10},    // 1 Jan 1972
-      {2287785600, 11},    // 1 Jul 1972
-      {2303683200, 12},    // 1 Jan 1973
-      {2335219200, 13},    // 1 Jan 1974
-      {2366755200, 14},    // 1 Jan 1975
-      {2398291200, 15},    // 1 Jan 1976
-      {2429913600, 16},    // 1 Jan 1977
-      {2461449600, 17},    // 1 Jan 1978
-      {2492985600, 18},    // 1 Jan 1979
-      {2524521600, 19},    // 1 Jan 1980
-      {2571782400, 20},    // 1 Jul 1981
-      {2603318400, 21},    // 1 Jul 1982
-      {2634854400, 22},    // 1 Jul 1983
-      {2698012800, 23},    // 1 Jul 1985
-      {2776982400, 24},    // 1 Jan 1988
-      {2840140800, 25},    // 1 Jan 1990
-      {2871676800, 26},    // 1 Jan 1991
-      {2918937600, 27},    // 1 Jul 1992
-      {2950473600, 28},    // 1 Jul 1993
-      {2982009600, 29},    // 1 Jul 1994
-      {3029443200, 30},    // 1 Jan 1996
-      {3076704000, 31},    // 1 Jul 1997
-      {3124137600, 32},    // 1 Jan 1999
-      {3345062400, 33},    // 1 Jan 2006
-      {3439756800, 34},    // 1 Jan 2009
-      {3550089600, 35},    // 1 Jul 2012
-      {3644697600, 36},    // 1 Jul 2015
-      {3692217600, 37}     // 1 Jan 2017
+  static int getLeapSeconds(int64_t current_timestamp) {
+    static const std::map<int64_t, int> leap_seconds =
+    {
+      {2272060800, 10},     // 1 Jan 1972
+      {2287785600, 11},     // 1 Jul 1972
+      {2303683200, 12},     // 1 Jan 1973
+      {2335219200, 13},     // 1 Jan 1974
+      {2366755200, 14},     // 1 Jan 1975
+      {2398291200, 15},     // 1 Jan 1976
+      {2429913600, 16},     // 1 Jan 1977
+      {2461449600, 17},     // 1 Jan 1978
+      {2492985600, 18},     // 1 Jan 1979
+      {2524521600, 19},     // 1 Jan 1980
+      {2571782400, 20},     // 1 Jul 1981
+      {2603318400, 21},     // 1 Jul 1982
+      {2634854400, 22},     // 1 Jul 1983
+      {2698012800, 23},     // 1 Jul 1985
+      {2776982400, 24},     // 1 Jan 1988
+      {2840140800, 25},     // 1 Jan 1990
+      {2871676800, 26},     // 1 Jan 1991
+      {2918937600, 27},     // 1 Jul 1992
+      {2950473600, 28},     // 1 Jul 1993
+      {2982009600, 29},     // 1 Jul 1994
+      {3029443200, 30},     // 1 Jan 1996
+      {3076704000, 31},     // 1 Jul 1997
+      {3124137600, 32},     // 1 Jan 1999
+      {3345062400, 33},     // 1 Jan 2006
+      {3439756800, 34},     // 1 Jan 2009
+      {3550089600, 35},     // 1 Jul 2012
+      {3644697600, 36},     // 1 Jul 2015
+      {3692217600, 37}      // 1 Jan 2017
     };
-    return leap_seconds;
+    int64_t timestamp_converted = convertUnixToNtp(current_timestamp);
+    auto result = leap_seconds.lower_bound(timestamp_converted);
+    // Here we handle the case where our input time is outside
+    // of our map bounds.
+    if (result == leap_seconds.end()) {
+      return std::prev(result)->second;
+    }
+    // Here we handle the case where our input time is within range of our
+    // map bounds and is not equal in time to a specific key.
+    if (result->first > timestamp_converted) {
+      return std::prev(result)->second;
+    }
+    // Here we handle the case where our input time is within range of our
+    // map bounds and is equal in time to a specific key.
+    return result->second;
   }
 };
